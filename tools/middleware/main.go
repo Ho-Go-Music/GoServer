ea305fcc997b2b30875ce2f6c0fbcc58c0a6ec1e
Acaibird <newcoder@icloud.com>
2024-04-12 08:40:10 +0800

    feat: specify  cache time of prefight request, By default, the browser sends a preflight request before every cross-domain request, unless the cache time of the preflight request has not expired.

--- tools/middleware/main.go
+++ tools/middleware/main.go
@@ -14,9 +14,13 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		// 检查请求是否为 OPTIONS 预检请求
 		// Check if the request is an OPTIONS preflight request
 		if r.Method == "OPTIONS" {
+			// Set additional headers for preflight request
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Max-Age", "86400") // 24 hours
 			w.WriteHeader(http.StatusOK)
 			return
 		}
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		// 继续处理下一个中间件或请求处理函数
 		next.ServeHTTP(w, r)
 	})
