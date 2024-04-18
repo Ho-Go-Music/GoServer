390016ca0fba2073984b7405d42da6377a4c8c54
Acaibird <durbannhawkey42@gmail.com>
2024-04-18 14:28:47 +0800

    feat:To store the login time for a user in Redis after successful login and use it for managing online users

--- redis/redis.go
+++ redis/redis.go
@@ -2,26 +2,16 @@ package redis
 
 import (
 	"github.com/redis/go-redis/v9"
-	"sync"
 )
-
-var (
-	once        sync.Once
-	redisClient *redis.Client
-)
-
-func InitRedisClient() {
-	once.Do(func() {
-		// 创建 Redis 客户端
-		redisClient = redis.NewClient(&redis.Options{
-			Addr:     "localhost:6379",
-			Password: "775028", // 如果密码已设置，请提供密码
-			DB:       0,        // 选择要使用的数据库
-		})
-	})
-}
 
 func GetRedisClient() *redis.Client {
-	InitRedisClient()
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:           "localhost:6379",
+		Password:       "775028", // 如果密码已设置，请提供密码
+		DB:             0,        // 选择要使用的数据库
+		MaxActiveConns: 100,
+		MaxIdleConns:   10,
+	})
+
 	return redisClient
 }
--- tools/httpfunc/httpfunc.go
+++ tools/httpfunc/httpfunc.go
@@ -4,9 +4,12 @@ import (
 	diylog "acaibird.com/log"
 	"acaibird.com/mysql"
 	"acaibird.com/mysql/table"
+	"acaibird.com/redis"
 	"acaibird.com/request_body"
 	sessions2 "acaibird.com/sessions"
+	"context"
 	"database/sql"
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/sessions"
@@ -64,16 +67,36 @@ func LogInHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 编码器（发送器）和解码器（接收器）之间进行二进制数据流的发送，
+	// 一般用来传递远端程序调用的参数和结果，比如net/rpc包就有用到这个
+	gob.Register(time.Time{})
 	session, err := sessions2.Store.Get(r, "session")
 	session.Values[user.Username] = time.Now()
 	session.Options = &sessions.Options{
 		MaxAge: 60 * 60 * 2,
 	}
-	session.Save(r, w)
+	err = session.Save(r, w)
+	if err != nil {
+		diylog.Sugar.Errorln("session save throw a error ", err)
+	}
+
+	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
+	defer cancel()
+	redisClient := redis.GetRedisClient()
+	_, err = redisClient.Set(ctx, user.Username, time.Now(), 60*time.Minute).Result()
+	if err != nil {
+		diylog.Sugar.Errorln("redis save online user info throw a error ", err)
+	}
+	diylog.Sugar.Infoln("redis save online user info successfully")
+
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("success"))
+
+	_, err = w.Write([]byte("success"))
+	if err != nil {
+		return
+	}
 }
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
