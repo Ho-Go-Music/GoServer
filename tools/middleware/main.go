620177cb0f29c779426fb1ffeb58510832c50dc6
Acaibird <durbannhawkey42@gmail.com>
2024-04-22 09:31:15 +0800

    feat:User authentication middleware, prior to reaching specific routes

--- tools/middleware/main.go
+++ tools/middleware/main.go
@@ -1,15 +1,19 @@
 package middleware
 
 import (
-	"fmt"
+	diylog "acaibird.com/log"
+	sessions2 "acaibird.com/sessions"
+	"encoding/gob"
 	"net/http"
+	"strings"
+	"time"
 )
 
 // CorsMiddleware is a middleware to set CORS headers
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// set allowed origin
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5174")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		// set allowed method
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		// set allowed header
@@ -21,6 +25,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		// Check if the request is an OPTIONS preflight request
 		if r.Method == "OPTIONS" {
 			// Set additional headers for preflight request
+			diylog.Sugar.Infoln("preflight request")
 			w.Header().Set("Access-Control-Max-Age", "86400") // 24 hours
 			w.WriteHeader(http.StatusOK)
 			return
@@ -30,11 +35,35 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-// AuthMiddleware is a middleware to check if the request is authorized
+// Identify AuthMiddleware is a middleware to check if the request is authorized
 func Identify(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		println(r.URL.Path)
-		fmt.Fprintf(w, "hello, %s", r.URL.Path)
+		gob.Register(time.Time{})
+		session, err := sessions2.Store.Get(r, "session")
+		if err != nil {
+			diylog.Sugar.Errorln("get sessions error: ", err)
+			http.Error(w, "StatusInternalServerError", http.StatusInternalServerError)
+			return
+		}
+		// There is a situation where the user has not logged in yet
+		// the /login routing request is directly handed over to the corresponding handler for processing.
+		if r.URL.Path == "/login" {
+			next.ServeHTTP(w, r)
+			return
+		}
+		// if session is new , user has not logged in
+		if session.IsNew {
+			print("new session")
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		// get username from query parameters
+		username := strings.Join(r.URL.Query()["username"], "")
+		// Check session integrity
+		if _, ok := session.Values[username]; !ok {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
